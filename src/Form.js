2adcc5992dc900dc45435dc138100e5059653647
GeralmyUrena <geralmyuri11@gmail.com>
2023-06-15 21:32:43 -0400

    created form component

---
+++ src/Form.js
@@ -0,0 +1,19 @@
+import React from "react"
+function Form (){
+
+    return (
+    <div>
+        <form>
+        <div>
+        <input required type="text" name="author" value="" placeholder="Author" />
+      </div>
+            {/* <input type="text" >Name</input>
+            <input type="date">DOB</input>
+            <input type="date">DOS</input> */}
+
+
+        </form>
+    </div>)
+}
+
+export default Form
