8837a950e57cc3b971a6cfc216dc59cc152d4a2e
GeralmyUrena <geralmyuri11@gmail.com>
2023-07-11 16:10:08 -0400

    deleted comments on addDoctorBTN

--- src/AddDoctorButton.js
+++ src/AddDoctorButton.js
@@ -1,14 +1,10 @@
 import React from 'react'
-import {useNavigate} from 'react-router-dom'
+
 import { Link } from 'react-router-dom'
 
 export default function AddDoctorButton() {
-    let history = useNavigate()
-    // function handleClick(e){
-    //   e.preventDefault()
-    //     history.push('/addDr')
-   
-    // }
+    
+  
   return (
     <Link to="/doctors/new">
       {/* <span class="material-symbols-outlined">Add</span>  */}
