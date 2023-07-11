36022b1f766a036d69b49c9d156affad1d496fe4
GeralmyUrena <geralmyuri11@gmail.com>
2023-07-11 16:11:32 -0400

    deleted comms on addDoctorBTN

--- src/AddDoctorButton.js
+++ src/AddDoctorButton.js
@@ -6,7 +6,7 @@ export default function AddDoctorButton() {
   
   return (
     <Link to="/doctors/new">
-      {/* <span class="material-symbols-outlined">Add</span>  */}
+    
 
       <button  type="button" className="btn btn-success" >+ Add doctor</button>
     </Link>
