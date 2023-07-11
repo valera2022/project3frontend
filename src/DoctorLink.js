1e1aa1fc47cc11b379ee093aa00739adcc4c18db
GeralmyUrena <geralmyuri11@gmail.com>
2023-07-11 16:03:09 -0400

    indentation on doctorlink

--- src/DoctorLink.js
+++ src/DoctorLink.js
@@ -4,46 +4,46 @@ import { useNavigate } from 'react-router-dom'
 
 
 
-export default function DoctorLink({doctor,handleDeleteDr,setSingleDr}) {
- 
+export default function DoctorLink({ doctor, handleDeleteDr, setSingleDr }) {
+
   //handleEditDrdefault is used to have default values on edit from and get dr id in edit form
   let navigate = useNavigate()
   console.log(doctor.id)
   //provides Id for mt dr patch
-  function handleClickEdit(e){
-  e.preventDefault()
-        console.log(doctor.id)
-       
-        setSingleDr(doctor)
-     
+  function handleClickEdit(e) {
+    e.preventDefault()
+    console.log(doctor.id)
 
-      navigate(`/doctors/${doctor.id}/edit`)
-     
+    setSingleDr(doctor)
+
+
+    navigate(`/doctors/${doctor.id}/edit`)
+
   }
 
-  function handleClickDelete(){
-       handleDeleteDr(doctor.id)
-      // handleDrId(doctor.id)
+  function handleClickDelete() {
+    handleDeleteDr(doctor.id)
+    // handleDrId(doctor.id)
 
-     
+
   }
-  
 
-    
-    console.log(doctor)
+
+
+  console.log(doctor)
   return (
-   <div className='drdiv'>
-    <br></br>
-      <Link style ={{textDecoration:"none"}}to={`/doctors/${doctor.id}`}>
-       <div style={{color:"whitesmoke"}}   ><p className='dlist'>Dr. { doctor.name}</p> </div>
-     </Link>
-    
+    <div className='drdiv'>
+      <br></br>
+      <Link style={{ textDecoration: "none" }} to={`/doctors/${doctor.id}`}>
+        <div style={{ color: "whitesmoke" }}   ><p className='dlist'>Dr. {doctor.name}</p> </div>
+      </Link>
+
       <button onClick={handleClickEdit} type="button" class="btn btn-outline-success btn-sm" >Edit</button>
       |
       <button onClick={handleClickDelete} type="button" class="btn btn-outline-danger btn-sm">Delete</button>
       {/* <DeleteDoctor handleOnDoctorDelete={handleOnDoctorDelete} /> */}
       {/* <button onClick={handleDelete}>Delete</button> */}
-      
+
     </div>
   )
 }
