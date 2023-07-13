41e6e22cb4bd204096efafbb73c295b898dc446c
GeralmyUrena <geralmyuri11@gmail.com>
2023-07-12 20:35:23 -0400

    delete comment on patient

--- src/Patient.js
+++ src/Patient.js
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom'
  function Patient({pt}) {
   console.log(pt)
 
-    // let ptName =  pt.map((p) => p.name )
+  
     // let ptId =  pts.map((p) => p.id )
 
   //  console.log(pt.name)
